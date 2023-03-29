73bda13947f96c441dfac1880a240ae3057ead07
kip-too <hoseakiptoo201@gmail.com>
2023-03-29 07:26:30 +0300

    Initial commit

---
+++ src/test/java/org/kiptoo/MsokoIT.java
@@ -0,0 +1,8 @@
+package org.kiptoo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class MsokoIT extends MsokoTest {
+    // Execute the same tests but in packaged mode.
+}
